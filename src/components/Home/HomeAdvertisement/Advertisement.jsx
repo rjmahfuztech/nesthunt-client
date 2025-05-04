fafb160f44064322d22b5e6b043b9bc9539e03e5
rjmahfuztech <rjmahfuztech@gmail.com>
2025-05-04 23:05:37 +0600

    advertisements data dynamically loaded and changed button design

--- src/components/Home/HomeAdvertisement/Advertisement.jsx
+++ src/components/Home/HomeAdvertisement/Advertisement.jsx
@@ -1,28 +1,41 @@
-import React from "react";
 import AdvertisementInfo from "./AdvertisementInfo";
 import { Swiper, SwiperSlide } from "swiper/react";
+
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/pagination";
-
-// import './styles.css';
-
 // import required modules
 import { Autoplay, FreeMode, Pagination } from "swiper/modules";
+import { useEffect, useState } from "react";
+import apiClient from "../../../services/apiClient";
 
 const Advertisement = () => {
+  const [advertisements, setAdvertisements] = useState([]);
+
   const customPagination = (_, className) => {
     return `<span class="${className} bg-gray-800 [&.swiper-pagination-bullet-active]:bg-gray-900 [&.swiper-pagination-bullet-active]:!opacity-100 !opacity-60"></span>`;
   };
 
+  useEffect(() => {
+    apiClient
+      .get("/advertisements")
+      .then((res) => (setAdvertisements(res.data), console.log(res.data)))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div className="max-w-[1350px] mx-auto px-4 my-20">
-      <h2 className="text-3xl font-bold text-center mb-4 uppercase">
+    <div id="advertisement" className="max-w-[1350px] mx-auto px-4 my-20">
+      <h2 className="text-2xl md:text-3xl font-bold text-center mb-4 uppercase">
         Check out our house advertisements
       </h2>
       <hr className="bg-orange-400 h-1 border-none w-48 mx-auto mb-14" />
       <div className="mt-5">
+        {advertisements.length === 0 && (
+          <div className="flex justify-center">
+            <div className="loader"></div>
+          </div>
+        )}
         <Swiper
           slidesPerView={1}
           spaceBetween={30}
@@ -48,48 +61,11 @@ const Advertisement = () => {
           modules={[Autoplay, FreeMode, Pagination]}
           className="mySwiper"
         >
-          <SwiperSlide>
-            <AdvertisementInfo />
-          </SwiperSlide>
-          <SwiperSlide>
-            <AdvertisementInfo />
-          </SwiperSlide>
-          <SwiperSlide>
-            <AdvertisementInfo />
-          </SwiperSlide>
-          <SwiperSlide>
-            <AdvertisementInfo />
-          </SwiperSlide>
-          <SwiperSlide>
-            <AdvertisementInfo />
-          </SwiperSlide>
-          <SwiperSlide>
-            <AdvertisementInfo />
-          </SwiperSlide>
-          <SwiperSlide>
-            <AdvertisementInfo />
-          </SwiperSlide>
-          <SwiperSlide>
-            <AdvertisementInfo />
-          </SwiperSlide>
-          <SwiperSlide>
-            <AdvertisementInfo />
-          </SwiperSlide>
-          <SwiperSlide>
-            <AdvertisementInfo />
-          </SwiperSlide>
-          <SwiperSlide>
-            <AdvertisementInfo />
-          </SwiperSlide>
-          <SwiperSlide>
-            <AdvertisementInfo />
-          </SwiperSlide>
-          <SwiperSlide>
-            <AdvertisementInfo />
-          </SwiperSlide>
-          <SwiperSlide>
-            <AdvertisementInfo />
-          </SwiperSlide>
+          {advertisements.map((advertisement) => (
+            <SwiperSlide key={advertisement.id}>
+              <AdvertisementInfo advertisement={advertisement} />
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </div>
--- src/components/Home/HomeAdvertisement/AdvertisementInfo.jsx
+++ src/components/Home/HomeAdvertisement/AdvertisementInfo.jsx
@@ -7,8 +7,9 @@ import {
   HouseRooms,
   Square3dThreePoints,
 } from "iconoir-react";
+import { Link } from "react-router";
 
-const AdvertisementInfo = () => {
+const AdvertisementInfo = ({ advertisement }) => {
   return (
     <div>
       <Card className="w-full max-w-[26rem] mx-auto shadow-lg">
@@ -27,38 +28,41 @@ const AdvertisementInfo = () => {
             <HeartSolid className="h-5 w-5" />
           </IconButton>
           <Typography className="!absolute left-2 bottom-2 font-semibold bg-slate-200 px-4 py-2 rounded-full">
-            $4200/Mo
+            {advertisement.rental_amount}/Mo
           </Typography>
         </Card.Header>
         <Card.Body>
           <div className="mb-2 flex items-center justify-between">
-            <Typography type="h6">Wooden House, Florida</Typography>
+            <Typography type="h6">{advertisement.title}</Typography>
             <Typography className="flex items-center gap-1.5 ">
               <StarSolid className="h-[18px] w-[18px] text-warning" />
               5.0
             </Typography>
           </div>
           <Typography className="text-foreground">
-            Enter a freshly updated and thoughtfully furnished peaceful home
-            surrounded by ancient trees, stone walls, and open meadows.
+            {advertisement.description.slice(0, 35)}...
           </Typography>
           <div className="group mt-6 inline-flex flex-wrap text-gray-500 items-center gap-3">
             <div className="flex gap-2 items-center">
               <HouseRooms strokeWidth={2} className="h-5 w-5" />
-              <span>2 Beds</span>
+              <span>{advertisement.bedroom} Beds</span>
             </div>
             <div className="flex gap-2 items-center">
               <Bathroom strokeWidth={2} className="h-5 w-5" />
-              <span>2 Baths</span>
+              <span>{advertisement.bathroom} Baths</span>
             </div>
             <div className="flex gap-2 items-center">
               <Square3dThreePoints strokeWidth={2} className="h-5 w-5" />
-              <span>240 sqft</span>
+              <span>{advertisement.apartment_size} sqft</span>
             </div>
           </div>
         </Card.Body>
         <Card.Footer className="pt-3">
-          <Button isFullWidth>Reserve</Button>
+          <Link to={`/advertisement/${advertisement.id}`}>
+            <Button className="w-full bg-green-600 px-8 py-3 font-bold hover:bg-green-700 border-none">
+              See Details
+            </Button>
+          </Link>
         </Card.Footer>
       </Card>
     </div>

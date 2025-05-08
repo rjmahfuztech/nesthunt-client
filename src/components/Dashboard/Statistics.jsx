ebf50c3e51285e466b20ae57c07dac74239f621d
rjmahfuztech <rjmahfuztech@gmail.com>
2025-05-08 22:08:23 +0600

    statistic detail added

--- src/components/Dashboard/Statistics.jsx
+++ src/components/Dashboard/Statistics.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Statistics = ({ stat }) => {
   return (
-    <div className="border rounded-md p-4">
+    <div className="border rounded-md p-4 bg-white shadow-sm">
       <h2 className="text-lg font-bold">{stat.title}</h2>
       <div className="flex items-center gap-2 mt-2">
         {<stat.icon className="w-7 h-7 text-green-500" />}

e779a56037fbc1ac7f9a976d9571d32e7e44ec3b
rjmahfuztech <rjmahfuztech@gmail.com>
2025-05-11 18:18:38 +0600

    dynamically data load using filter

--- src/pages/Rentals.jsx
+++ src/pages/Rentals.jsx
@@ -1,22 +1,19 @@
-import React, { useEffect, useState } from "react";
-import apiClient from "../services/apiClient";
 import AdvertisementInfo from "../components/Home/HomeAdvertisement/AdvertisementInfo";
 import FilterSection from "../components/FilterSection/FilterSection";
+import useFetchAdvertisement from "../hooks/useFetchAdvertisement";
+import useAppContext from "../hooks/useAppContext";
 
 const Rentals = () => {
-  const [rentals, setRentals] = useState([]);
-  const [loading, setLoading] = useState(false);
+  // import from AppContext
+  const { filter, register, handleSubmit, onSubmit, control } = useAppContext();
 
-  useEffect(() => {
-    setLoading(true);
-    apiClient
-      .get("/advertisements/")
-      .then((res) => setRentals(res.data))
-      .catch((err) => console.log(err))
-      .finally(() => setLoading(false));
-  }, []);
+  const { advertisements, loading } = useFetchAdvertisement({
+    category: filter?.category || "",
+    location: filter?.location || "",
+    bedroom: filter?.bedroom || "",
+    bathroom: filter?.bathroom || "",
+  });
 
-  console.log(rentals);
   return (
     <div className="max-w-[1350px] mx-auto px-4">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-0 sm:gap-4 md:gap-6">
@@ -25,17 +22,20 @@ const Rentals = () => {
             All Rentals
           </h2>
           {/* Loading */}
-          {loading && (
-            <div className="flex justify-center mt-32">
+          {loading ? (
+            <div className="flex justify-center my-20 md:my-32">
               <div className="loader"></div>
             </div>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6 mb-10 md:mb-20">
+              {advertisements.map((advertisement) => (
+                <AdvertisementInfo
+                  key={advertisement.id}
+                  advertisement={advertisement}
+                />
+              ))}
+            </div>
           )}
-          {/* advertisement data  */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6 mb-10 md:mb-20">
-            {rentals.map((rental) => (
-              <AdvertisementInfo key={rental.id} advertisement={rental} />
-            ))}
-          </div>
         </div>
         {/* Filter section  */}
         <div className="relative order-1 md:order-2">
@@ -43,7 +43,12 @@ const Rentals = () => {
             <h2 className="text-3xl font-bold uppercase my-10 max-w-80 border-b-2 border-b-orange-400">
               Search Here
             </h2>
-            <FilterSection />
+            <FilterSection
+              register={register}
+              handleSubmit={handleSubmit}
+              onSubmit={onSubmit}
+              control={control}
+            />
           </div>
         </div>
       </div>
